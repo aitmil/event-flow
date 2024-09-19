9163bdddfbf4b5c81fb032a1d14feb03f6b8320d
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-19 16:06:25 +0300

    update routes

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -1,37 +1,31 @@
+import express from 'express';
 import { Router } from 'express';
-import Participant from '../db/models/participant.js';
-import { fetchEvents } from '../controllers/events.js';
+import { getAllEventsController } from '../controllers/events.js';
+import {
+  getParticipantsController,
+  registerParticipantController,
+} from '../controllers/participants.js';
+import { registerParticipantSchema } from '../validation/participants.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
+import { validateBody } from '../middlewares/validateBody.js';
+import { isValidId } from '../middlewares/isValidId.js';
 
 const router = Router();
+const jsonParser = express.json();
 
-router.get('/events', ctrlWrapper(fetchEvents));
+router.get('/events', ctrlWrapper(getAllEventsController));
 
-router.post('/register', async (req, res) => {
-  const participant = {
-    fullName: req.body.fullName,
-    email: req.body.email,
-    dateOfBirth: req.body.dateOfBirth,
-    source: req.body.source,
-    eventId: req.event._id,
-  };
+router.post(
+  '/register',
+  jsonParser,
+  validateBody(registerParticipantSchema),
+  ctrlWrapper(registerParticipantController)
+);
 
-  const result = new Participant(participant);
-  await result.save();
-  res.status(201).json({
-    status: 200,
-    message: 'Participant of event registered successfully!',
-    data: participant,
-  });
-});
-
-router.get('/participants/:eventId', async (req, res) => {
-  const participants = await Participant.find({ eventId: req.params.eventId });
-  res.status(200).json({
-    status: 200,
-    message: 'Participants of event retrieved successfully!',
-    data: participants,
-  });
-});
+router.get(
+  '/participants/:eventId',
+  isValidId,
+  ctrlWrapper(getParticipantsController)
+);
 
 export default router;
