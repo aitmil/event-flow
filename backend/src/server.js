339bba8ee12b3f9304357b910922fb0cb42805bc
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-19 13:32:38 +0300

    init server and bd

---
+++ backend/src/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+import cors from 'cors';
+import pino from 'pino-http';
+
+import { env } from './utils/env.js';
+import router from './routers/index.js';
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/errorFoundHandler.js';
+
+const PORT = Number(env('PORT', '3000'));
+
+if (isNaN(PORT)) {
+  throw new Error('Invalid port number');
+}
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    })
+  );
+
+  app.use(router);
+
+  app.use(notFoundHandler);
+
+  app.use(errorHandler);
+
+  app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+};
