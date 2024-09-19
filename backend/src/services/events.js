55826635314ad91f863897522aa866972b294793
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-19 13:34:55 +0300

    add events

---
+++ backend/src/services/events.js
@@ -0,0 +1,29 @@
+import { SORT_ORDER } from '../utils/parseSortParams.js';
+import { calculatePaginationData } from '../utils/calculatePaginationData.js';
+import Event from '../db/models/event.js';
+
+export const getAllEvents = async ({
+  page = 1,
+  perPage = 10,
+  sortOrder = SORT_ORDER.ASC,
+  sortBy = 'eventDate',
+}) => {
+  const limit = perPage;
+  const skip = page > 0 ? (page - 1) * perPage : 0;
+
+  const eventsQuery = Event.find();
+
+  const sortingCriteria = { [sortBy]: sortOrder };
+
+  const [events, count] = await Promise.all([
+    eventsQuery.sort(sortingCriteria).skip(skip).limit(limit).exec(),
+    Event.countDocuments(),
+  ]);
+
+  const paginationData = calculatePaginationData(count, perPage, page);
+
+  return {
+    data: events,
+    ...paginationData,
+  };
+};
