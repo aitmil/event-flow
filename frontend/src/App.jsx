6f0ccca98421cb245b9181f8d87ad7f0825c63ee
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-20 02:57:39 +0300

    init project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Suspense, lazy } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import 'modern-normalize';
+import Loader from './components/Loader/Loader';
 
-function App() {
-  const [count, setCount] = useState(0)
+const EventsPage = lazy(() => import('./pages/EventsPage/EventsPage'));
+const RegisterPage = lazy(() => import('./pages/RegisterPage/RegisterPage'));
+const ParticipantsPage = lazy(() =>
+  import('./pages/ParticipantsPage/ParticipantsPage')
+);
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
+export default function App() {
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<EventsPage />} />
+          <Route path="/register/:eventId" element={<RegisterPage />} />
+          <Route path="/participants/:eventId" element={<ParticipantsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </Suspense>
     </>
-  )
+  );
 }
-
-export default App
