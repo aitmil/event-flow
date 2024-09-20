31a15cafe4b51bdceaff419557cdc5b44fdd6746
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-20 17:15:32 +0300

    add sorting

--- frontend/src/pages/EventsPage/EventsPage.jsx
+++ frontend/src/pages/EventsPage/EventsPage.jsx
@@ -1,23 +1,31 @@
 import { useState, useEffect } from 'react';
 import toast, { Toaster } from 'react-hot-toast';
-import { Pagination } from 'antd';
+import { Pagination, Select } from 'antd';
 
 import Loader from '../../components/Loader/Loader';
 import EventsList from '../../components/EventsList/EventsList';
 import { fetchEvents } from '../../api';
 import css from './EventsPage.module.css';
 
+const { Option } = Select;
+
 export default function EventsPage() {
   const [events, setEvents] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [totalItems, setTotalItems] = useState(1);
+  const [sortBy, setSortBy] = useState('eventDate');
+  const [sortOrder, setSortOrder] = useState('asc');
 
   useEffect(() => {
     const getAllEvents = async () => {
       try {
         setIsLoading(true);
-        const { events, totalItems } = await fetchEvents(page);
+        const { events, totalItems } = await fetchEvents(
+          page,
+          sortBy,
+          sortOrder
+        );
         setEvents(events);
         setTotalItems(totalItems);
         if (!events.length) {
@@ -35,11 +43,45 @@ export default function EventsPage() {
       }
     };
     getAllEvents();
-  }, [page]);
+  }, [page, sortBy, sortOrder]);
+
+  const handleSortByChange = value => {
+    setSortBy(value);
+    setPage(1);
+  };
+
+  const handleSortOrderChange = value => {
+    setSortOrder(value);
+    setPage(1);
+  };
 
   return (
     <main className={css.container}>
       {isLoading && <Loader />}
+
+      <h1 className={css.header}>Upcoming Events</h1>
+
+      <div className={css.sort}>
+        <Select
+          defaultValue="eventDate"
+          onChange={handleSortByChange}
+          style={{ width: 180, marginRight: '10px' }}
+        >
+          <Option value="eventDate">Event Date</Option>
+          <Option value="title">Title</Option>
+          <Option value="organizer">Organizer</Option>
+        </Select>
+
+        <Select
+          defaultValue="asc"
+          onChange={handleSortOrderChange}
+          style={{ width: 180 }}
+        >
+          <Option value="asc">Ascending</Option>
+          <Option value="desc">Descending</Option>
+        </Select>
+      </div>
+
       <EventsList events={events} />
       <Pagination
         current={page}
@@ -48,7 +90,7 @@ export default function EventsPage() {
           setPage(newPage);
         }}
         showSizeChanger={false}
-        style={{ textAlign: 'center', marginTop: '20px' }}
+        className={css.pagination}
       />
       <Toaster
         toastOptions={{
