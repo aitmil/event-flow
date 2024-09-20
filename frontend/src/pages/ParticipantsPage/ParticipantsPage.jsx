805ebf48f02127f1db466b544be249861a4e618f
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-20 17:16:45 +0300

    add backlink

--- frontend/src/pages/ParticipantsPage/ParticipantsPage.jsx
+++ frontend/src/pages/ParticipantsPage/ParticipantsPage.jsx
@@ -1,5 +1,20 @@
+import { useRef } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { HiArrowLeft } from 'react-icons/hi';
 import css from './ParticipantsPage.module.css';
 
 export default function ParticipantsPage() {
-  <></>;
+  const { eventId } = useParams();
+  const location = useLocation();
+  const backLinkHref = useRef(location.state ?? '/');
+
+  return (
+    <main className={css.container}>
+      <Link to={backLinkHref.current} className={css.backLink}>
+        <HiArrowLeft /> Go back
+      </Link>
+      <h1 className={css.header}>Event Participants</h1>
+      {/* <ParticipantsList eventId={eventId} /> */}
+    </main>
+  );
 }
--- frontend/src/pages/RegisterPage/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage/RegisterPage.jsx
@@ -1,16 +1,23 @@
 // RegistrationPage.jsx
-import React from 'react';
-import { useParams } from 'react-router-dom';
-import RegisterContent from '../../components/RegisterContent';
+import { useRef } from 'react';
+import { Link, useLocation, useParams } from 'react-router-dom';
+import { HiArrowLeft } from 'react-icons/hi';
+import RegisterContent from '../../components/RegisterContent/RegisterContent';
+import css from './RegisterPage.module.css';
 
 const RegistrationPage = () => {
-  const { eventId } = useParams(); // Get event ID from URL parameters
+  const { eventId } = useParams();
+  const location = useLocation();
+  const backLinkHref = useRef(location.state ?? '/');
 
   return (
-    <div>
-      <h1>Event Registration</h1>
+    <main className={css.container}>
+      <Link to={backLinkHref.current} className={css.backLink}>
+        <HiArrowLeft /> Go back
+      </Link>
+      <h1 className={css.header}>Event Registration</h1>
       <RegisterContent eventId={eventId} />
-    </div>
+    </main>
   );
 };
 
