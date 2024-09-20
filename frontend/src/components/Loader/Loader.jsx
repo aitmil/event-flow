c8d95841779512fe2c62db7312d442136446b0c2
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-20 17:18:03 +0300

    fix styles

--- frontend/src/components/Loader/Loader.jsx
+++ frontend/src/components/Loader/Loader.jsx
@@ -8,7 +8,7 @@ export default function Loader() {
         visible={true}
         height="100"
         width="100"
-        color="#7a1f50"
+        color="#1677ff"
         radius="9"
         ariaLabel="three-dots-loading"
         wrapperClass={css.loader}
