7019679c73d153e7aa93b10b6b6ff65e0de5857a
Anna Taranchuk <taranchuk4@yahoo.com>
2024-09-21 03:10:45 +0300

    clean code

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
-// import 'antd/dist/antd.css';
 import App from './App.jsx';
 import './index.css';
 
