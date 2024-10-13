9cb032e7bb1b45a0e2fdfbb149aaa917fe439e72
Anna Taranchuk <taranchuk4@yahoo.com>
2024-10-13 12:20:54 +0300

    add filter to fetchEvents

--- backend/src/services/events.js
+++ backend/src/services/events.js
@@ -1,6 +1,7 @@
 import { SORT_ORDER } from '../utils/parseSortParams.js';
 import { calculatePaginationData } from '../utils/calculatePaginationData.js';
 import Event from '../db/models/event.js';
+import dayjs from 'dayjs';
 
 export const getAllEvents = async ({
   page = 1,
@@ -11,13 +12,17 @@ export const getAllEvents = async ({
   const limit = perPage;
   const skip = page > 0 ? (page - 1) * perPage : 0;
 
-  const eventsQuery = Event.find();
+  const today = dayjs().startOf('day').toDate();
+
+  const eventsQuery = Event.find({
+    eventDate: { $gte: today },
+  });
 
   const sortingCriteria = { [sortBy]: sortOrder };
 
   const [events, count] = await Promise.all([
     eventsQuery.sort(sortingCriteria).skip(skip).limit(limit).exec(),
-    Event.countDocuments(),
+    Event.countDocuments({ eventDate: { $gte: today } }),
   ]);
 
   const paginationData = calculatePaginationData(count, perPage, page);
